a4c124bf233f7c8a5f3779f8aba954f03c0a83e4
Christophe Arendt <christophe.arendt@edu.escpeurope.eu>
2021-12-08 01:45:45 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
 with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
@@ -13,5 +14,4 @@ setup(name='Monuments',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/TaxiFareModel-run'],
       zip_safe=False)
